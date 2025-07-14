a67e7139d9c2df8a1b241eee96aa44d59d226658
krosengr4 <rosenkev4@gmail.com>
2025-07-14 10:56:12 -0500

    Batman

---
+++ yallmart/src/main/java/com/example/yallmart/YallmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.yallmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YallmartApplication.class, args);
+	}
+
+}
---
+++ yallmart/src/test/java/com/example/yallmart/YallmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yallmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
