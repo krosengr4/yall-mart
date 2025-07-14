09532bd56c3227c4ea14d628aee07281f115a3f1
krosengr4 <rosenkev4@gmail.com>
2025-07-14 11:09:43 -0500

    Added dependencies

--- yallmart/src/main/java/com/example/yallmart/YallmartApplication.java
+++ yallmart/src/main/java/com/example/yallmart/YallmartApplication.java
@@ -8,6 +8,8 @@ public class YallmartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YallmartApplication.class, args);
+
+		System.out.println("Y'ALL COME BACK NOW YA HEAR!!!");
 	}
 
 }
