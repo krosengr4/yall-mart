5e281dc2031b413366be750e73215869e75eb336
krosengr4 <rosenkev4@gmail.com>
2025-07-24 01:19:31 -0500

    Coded OrderDao interface

--- yallmart/src/main/java/com/pluralsight/yallmart/data/OrderDao.java
+++ yallmart/src/main/java/com/pluralsight/yallmart/data/OrderDao.java
@@ -1,4 +1,17 @@
 package com.pluralsight.yallmart.data;
 
+import com.pluralsight.yallmart.models.CartItem;
+import com.pluralsight.yallmart.models.Order;
+
+import java.util.List;
+
 public interface OrderDao {
+
+	Order getById(int orderId);
+
+	List<CartItem> getItemsOrdered(int userId);
+
+	void insertLineItems(CartItem cartItem, int orderId);
+
+	Order addOrder(Order order);
 }
