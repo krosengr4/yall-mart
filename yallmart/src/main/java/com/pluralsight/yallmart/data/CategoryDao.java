386c35bd0625250212928cb0dde96980635071cf
Phoniix <seanasb52058@gmail.com>
2025-07-23 23:16:17 -0500

    hello world

--- yallmart/src/main/java/com/pluralsight/yallmart/data/CategoryDao.java
+++ yallmart/src/main/java/com/pluralsight/yallmart/data/CategoryDao.java
@@ -1,4 +1,18 @@
 package com.pluralsight.yallmart.data;
 
+import com.pluralsight.yallmart.models.Category;
+
+import java.util.List;
+
 public interface CategoryDao {
+
+    List<Category> getAll();
+
+    Category getById(int categoryId);
+
+    Category create(Category category);
+
+    void update(int categoryId, Category category);
+
+    void delete(int categoryId);
 }
--- yallmart/src/main/java/com/pluralsight/yallmart/data/mysql/MySqlProductDao.java
+++ yallmart/src/main/java/com/pluralsight/yallmart/data/mysql/MySqlProductDao.java
@@ -1,4 +1,71 @@
 package com.pluralsight.yallmart.data.mysql;
 
-public class MySqlProductDao {
+import com.pluralsight.yallmart.data.ProductDao;
+import com.pluralsight.yallmart.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
+
+    @Autowired
+    public MySqlProductDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public List<Product> getAll () {
+        List<Product> allProducts = new ArrayList<>();
+        String query = """
+                SELECT * FROM products;
+                """;
+        try (Connection connection = getConnection()) {
+            ResultSet rs = connection.prepareStatement(query).executeQuery();
+            while (rs.next()) {
+            allProducts.add(rsHelper(rs));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error getting products");
+        }
+        return allProducts;
+    }
+
+    public Product getById (int productId) {
+        Product product = null;
+        String query = """
+                SELECT * FROM products
+                WHERE product_id = ?
+                """;
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, productId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                product = rsHelper(rs);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        return product;
+    }
+
+    Product rsHelper (ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("product_id");
+        String name = resultSet.getString("name");
+        BigDecimal price = resultSet.getBigDecimal("price");
+        int categoryId = resultSet.getInt("category_id");
+        String desc = resultSet.getString("description");
+        int stock = resultSet.getInt("stock");
+        return new Product(id, name, price, categoryId, desc, stock);
+    }
 }
