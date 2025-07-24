13c203301be5e13b5684d4a0f22327764f939cda
Phoniix <seanasb52058@gmail.com>
2025-07-24 11:56:13 -0500

    ProductDAO complete

--- yallmart/src/main/java/com/pluralsight/yallmart/data/ProductDao.java
+++ yallmart/src/main/java/com/pluralsight/yallmart/data/ProductDao.java
@@ -1,4 +1,16 @@
 package com.pluralsight.yallmart.data;
 
+import com.pluralsight.yallmart.models.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getAll ();
+    Product getById (int productId);
+    List<Product> search (Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> listByCategoryId (Integer categoryId);
+    Product create (Product product);
+    void update (int productId, Product product);
+    void delete (int productId);
 }
