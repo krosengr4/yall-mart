5cc3945ec175190f9077fa78809264540bedfec2
krosengr4 <rosenkev4@gmail.com>
2025-07-24 01:17:55 -0500

    Coded CartDao interface

--- yallmart/src/main/java/com/pluralsight/yallmart/data/CartDao.java
+++ yallmart/src/main/java/com/pluralsight/yallmart/data/CartDao.java
@@ -1,4 +1,20 @@
 package com.pluralsight.yallmart.data;
 
+import com.pluralsight.yallmart.models.Cart;
+import com.pluralsight.yallmart.models.CartItem;
+
+import java.util.List;
+
 public interface CartDao {
+
+	Cart getByUserId(int userId);
+
+	List<CartItem> getItemsInCart(int userId, int productId);
+
+	Cart addToCart(int userId, int productId);
+
+	void updateCart(int userId, int productId, CartItem cartItem);
+
+	void clearCart(int userId);
+
 }
